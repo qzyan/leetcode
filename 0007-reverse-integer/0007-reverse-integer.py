6f5d1008bfcc404b2c27c11f064afcfcc5212416
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-10-20 10:20:28 -1000

    Time: 54 ms (64.61%), Space: 13.9 MB (64.32%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,5 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        INT_MAX = 2**31 - 1
         result = 0
         isNeg = False
         
@@ -11,13 +12,17 @@ class Solution:
         
         while x > 0:
             remainder = x % 10
+            
+            # see if the result will be in range when * 10 + remainder
+            if result > INT_MAX / 10:
+                return 0
+            if result == INT_MAX // 10 and remainder > 7:
+                return 0
+                
             result = result * 10 + (remainder)
             x = x // 10
         
         if isNeg:
             result = -result
-        
-        if result > 2**31 - 1 or result < -(2**31):
-            return 0
         
         return result
