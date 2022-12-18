5ebc3ea63c5fa0fb5e4d7370cfb3acab5c6ebdd8
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-12-18 10:57:00 -0800

    Time: 37 ms (94.18%), Space: 15.2 MB (87.21%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,20 +9,24 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        self.flatten_return_last_node(root)
-        return root
-    
-    def flatten_return_last_node(self, root):
-        if not root:
-            return None
+        # iterate over each node -> node.right
+            # if node has no left node, continue
+            # find the right_most of the left node
+            # append the right node to right_most.right
+            # node.right = node.left
+            # node.left = None
         
-        left_last_node = self.flatten_return_last_node(root.left)
-        right_last_node = self.flatten_return_last_node(root.right)
-
-        if left_last_node:
-            left_last_node.right = root.right
+        while root:
+            if root.left is None:
+                root = root.right
+                continue
+            
+            node = root.left
+            while node.right:
+                node = node.right
+            node.right = root.right
+            
             root.right = root.left
             root.left = None
-
-        return right_last_node or left_last_node or root
-        
+            
+            root = root.right
