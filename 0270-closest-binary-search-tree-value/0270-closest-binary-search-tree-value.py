468dc9ebbd1affab8b64bd037f47ad83d2d963aa
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-12-18 13:25:26 -0800

    Time: 86 ms (38.24%), Space: 16.1 MB (85.06%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -6,15 +6,20 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        if root is None:
-            return None
+        curr = root
+        low_bound = None
+        high_bound = None
         
-        if root.val > target:
-            subtree_closest = self.closestValue(root.left, target)
-        else:
-            subtree_closest = self.closestValue(root.right, target)
-            
-        if subtree_closest is None:
-            return root.val
-            
-        return root.val if abs(root.val - target) < abs(subtree_closest - target) else subtree_closest
+        while curr:
+            if curr.val > target:
+                high_bound = curr.val
+                curr = curr.left
+            else:
+                low_bound = curr.val
+                curr = curr.right
+                
+        if low_bound is None:
+            return high_bound
+        if high_bound is None:
+            return low_bound
+        return low_bound if abs(low_bound - target) < abs(high_bound - target) else high_bound
