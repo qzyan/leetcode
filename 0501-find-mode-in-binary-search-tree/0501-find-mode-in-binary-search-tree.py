48e0bf1c6124ad6c3d496eb3c9db497554a4001d
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-12-20 18:00:13 -0800

    Time: 49 ms (98.56%), Space: 17.9 MB (91.61%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,34 +6,37 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        stack = []
-        curr = root
-        max_count = 0
-        curr_count = 0
-        prev_val = None
-        vals = []
+        recorder = {
+            "max_count": 0,
+            "curr_count": 0,
+            "prev_val": None
+        }
         
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            
-            curr = stack.pop()
-
-            if prev_val == curr.val:
-                curr_count += 1
-            if prev_val != curr.val:
-                curr_count = 1
-            if curr_count == max_count:
-                vals.append(curr.val)
-            elif curr_count > max_count:
-                vals = [curr.val]
-                max_count = curr_count
-            
-            prev_val = curr.val
-            if curr.right:
-                curr = curr.right
-            else:
-                curr = None
-                       
+        vals = []
+        self.inorder_traverse(root, recorder, vals)
         return vals
+        
+    def inorder_traverse(self, root, recorder, vals):
+        if root is None:
+            return 
+        
+        self.inorder_traverse(root.left, recorder, vals)
+        
+        if recorder["prev_val"] == root.val:
+            recorder["curr_count"] += 1
+        else:
+            recorder["curr_count"] = 1
+            
+        if recorder["curr_count"] == recorder["max_count"]:
+            vals.append(root.val)
+        elif recorder["curr_count"] > recorder["max_count"]:
+            vals.clear()
+            vals.append(root.val)
+            recorder["max_count"] = recorder["curr_count"]
+        
+        recorder["prev_val"] = root.val
+        
+        self.inorder_traverse(root.right, recorder, vals)
+        return            
+        
+            
