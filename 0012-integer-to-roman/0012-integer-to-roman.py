380c272ffd598d12f07296c2d9de14bc16f9f9b7
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-12-29 09:49:32 -0800

    Time: 56 ms (81.70%), Space: 14 MB (35.70%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -3,43 +3,31 @@ class Solution:
         if not num:
             return ''
         
-        val_symbol_map = {
-            1: 'I',
-            5: 'V',
-            10: 'X',
-            50: 'L',
-            100: 'C',
-            500: 'D',
-            1000: 'M'
-        }
+        val_symbols = [
+            (1, 'I'),
+            (4, 'IV'),
+            (5, 'V'),
+            (9, 'IX'),
+            (10, 'X'),
+            (40, 'XL'),
+            (50, 'L'),
+            (90, 'XC'),
+            (100, 'C'),
+            (400, 'CD'),
+            (500, 'D'),
+            (900, 'CM'),
+            (1000, 'M')
+        ]
+        val_symbols.sort(reverse=True)
+        print()
         
         romans = []
         
-        while num:
-            if num >= 1000:
-                unit = 1000
-            elif 100 <= num < 1000:
-                unit = 100
-            elif 10 <= num < 100:
-                unit = 10
-            elif 1<= num < 10:
-                unit = 1
-            
-            
+        for unit, symbol in val_symbols:
             count = num // unit
-            num -= count * unit
-            if count == 4:
-                romans.append(val_symbol_map[unit])
-                romans.append(val_symbol_map[unit * 5])
-            elif count == 9:
-                romans.append(val_symbol_map[unit])
-                romans.append(val_symbol_map[unit * 10])
-            elif count < 4:
-                for _ in range(count):
-                    romans.append(val_symbol_map[unit])
-            elif count < 9:
-                romans.append(val_symbol_map[unit * 5])
-                for _ in range(count - 5):
-                    romans.append(val_symbol_map[unit]) 
-        print(romans)
+            romans.append(symbol * count)
+            num = num % unit
+            if num == 0:
+                break
+                
         return ''.join(romans)
