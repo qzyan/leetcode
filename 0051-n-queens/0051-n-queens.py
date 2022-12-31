fa0f0c24b7ad8e4f49f0d97d26a73074d3eeda17
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2022-12-30 19:53:41 -0800

    Time: 69 ms (80.12%), Space: 14.4 MB (85.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,46 +1,42 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         solution = [["."] * n for _ in range(n)]
-        solutions = [] 
-        self.backtrack(solution, solutions, 0, n)
+        solutions = []
+        cols = set()
+        diags = set()
+        anti_diags = set()
+        self.backtrack(solution, solutions, 0, n, cols, diags, anti_diags)
         return solutions
         
-    def backtrack(self, solution, solutions, row_idx, n):
+    def backtrack(self, solution, solutions, row_idx, n, cols, diags, anti_diags):
         if row_idx == n:
             solutions.append(list(map(lambda eles: ''.join(eles), solution)))
             return
         
         for col_idx in range(n):
-            solution[row_idx][col_idx] = 'Q'
-            if not self.possible(solution, row_idx, col_idx, n):
+            if not self.possible(solution, row_idx, col_idx, n, cols, diags, anti_diags):
                 solution[row_idx][col_idx] = '.'
                 continue
             
-            self.backtrack(solution, solutions, row_idx + 1, n)
+            solution[row_idx][col_idx] = 'Q'
+            cols.add(col_idx)
+            diags.add(row_idx + col_idx)
+            anti_diags.add(row_idx - col_idx)
+            self.backtrack(solution, solutions, row_idx + 1, n, cols, diags, anti_diags)
             solution[row_idx][col_idx] = '.'
+            cols.remove(col_idx)
+            diags.remove(row_idx + col_idx)
+            anti_diags.remove(row_idx - col_idx)
             
         return
     
-    def possible(self, solution, row_ori, col_ori, n):
-        for row_idx in range(row_ori):
-            if solution[row_idx][col_ori] == 'Q':
-                return False
-            
-        
-        for row_idx in range(row_ori):
-            col_idx = row_ori + col_ori - row_idx
-            if self.inbound(row_idx, col_idx, n) and solution[row_idx][col_idx] == 'Q':
-                return False
-        
-        for row_idx in range(row_ori):
-            col_idx = row_idx - (row_ori - col_ori)
-            if self.inbound(row_idx, col_idx, n) and solution[row_idx][col_idx] == 'Q':
-                return False
+    def possible(self, solution, row_ori, col_ori, n, cols, diags, anti_diags):
+        diag = row_ori + col_ori
+        anti_diag = row_ori - col_ori
+        if col_ori in cols or diag in diags or anti_diag in anti_diags:
+            return False
         
         return True
-            
-    def inbound(self, row_idx, col_idx, n):
-        return 0 <= row_idx < n and 0 <= col_idx < n 
         
             
                 
