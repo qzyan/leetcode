294afd8b118e93e56880bb9875cbbf866c7f3d1a
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2023-01-10 00:54:37 -0800

    Time: 989 ms (75.17%), Space: 84.7 MB (15.10%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,6 +1,6 @@
 class ListNode:
-    def __init__(self, value):
-        self.val = value
+    def __init__(self, val):
+        self.val = val
         self.next = None
 
 class FirstUnique:
@@ -8,43 +8,49 @@ class FirstUnique:
     def __init__(self, nums: List[int]):
         self.dummy = ListNode(-1)
         self.tail = self.dummy
-        self.num_count = {}
-        self.num_prev = {}
+        self.unique_val_prev = {}
+        self.dups = set()
         
         for num in nums:
             self.add(num)
         
-        
+
     def showFirstUnique(self) -> int:
-        if self.dummy.next is None:
-            return -1
-        
-        return self.dummy.next.val
+        if self.dummy.next:
+            return self.dummy.next.val
+        return -1
 
     def add(self, value: int) -> None:
-        count = self.num_count.get(value, 0)
-        if count == 0:
-            node = ListNode(value)
-            self.num_prev[value] = self.tail
-            self.tail.next = node
-            self.tail = node
-        elif count == 1:
-            prev_node = self.num_prev[value]
-            curr_node = prev_node.next
-            prev_node.next = curr_node.next
-            curr_node.next = None
-            
-            # node to delete is not the tail
-            if prev_node.next:
-                self.num_prev[prev_node.next.val] = prev_node
-            else:
-                # node to delete is the tail
-                self.tail = prev_node
-            self.num_prev.pop(value)
+        if value in self.dups:
+            return
         
-        self.num_count[value] = count + 1
-            
-
+        # value was unique, with current add, is no more unique 
+        if value in self.unique_val_prev:
+            # remove the node from linked list
+            prev_node = self.unique_val_prev[value]
+            curr_node = prev_node.next
+            print(prev_node.val, curr_node.val)
+            prev_node.next = curr_node.next
+            # if the curr is tail, move the tail forward
+            if self.tail == curr_node:
+                self.tail = prev_node
+            # curr is not tail, update the unique_val_prev[next_node.val]
+            else:
+                next_val = curr_node.next.val
+                self.unique_val_prev[next_val] = prev_node
+                
+            # remove it from unique_val_prev
+            self.unique_val_prev.pop(value)
+            # add it to dups
+            self.dups.add(value)
+            return
+        
+        # value was never before
+        curr_node = ListNode(value)
+        self.tail.next = curr_node
+        self.unique_val_prev[value] = self.tail
+        self.tail = curr_node
+        return
 
 # Your FirstUnique object will be instantiated and called as such:
 # obj = FirstUnique(nums)
