3c8e31a1084dc3745e6ec7a5d62615340dbc9cfc
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2023-01-11 23:05:45 -0800

    Time: 834 ms (81.30%), Space: 20.2 MB (93.84%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,35 +1,17 @@
+import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        copied_points = points[:]
-        return self.quick_select(copied_points, 0, len(copied_points) - 1, k)
-        
-    
-    # get the k smallest 
-    def quick_select(self, points, start, end, k):
-        if start >= end:
-            return points[:k]
-        
-        left = start
-        right = end
-        mid = (left + right) // 2
-        pivot = points[mid]
-        
-        while left <= right:
-            while left <= right and self.calc_dist(points[left]) < self.calc_dist(pivot):
-                left += 1
-            while left <= right and self.calc_dist(points[right]) > self.calc_dist(pivot):
-                right -= 1
+        heap = []
+        heapq.heapify(heap)
+        for point in points:
+            dist = self.calc_dist(point)
+            heapq.heappush(heap, (-dist, -point[0], -point[1]))
             
-            if left <= right:
-                points[left], points[right] = points[right], points[left]
-                left += 1
-                right -= 1
-                
-        if k - 1 >= left:
-            return self.quick_select(points, left, end, k)
-        if k - 1 <= right:
-            return self.quick_select(points, start, right, k)
-        return points[:k]
-    
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        
+        return map(lambda ele: (-ele[1], -ele[2]), heap)
+            
     def calc_dist(self, point):
         return point[0] ** 2 + point[1] ** 2
