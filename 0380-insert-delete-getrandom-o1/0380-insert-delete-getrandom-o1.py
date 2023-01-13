d6c674271aba18ef3f6dac24664e0dce5e580d11
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2023-01-12 17:26:04 -0800

    Time: 501 ms (79.66%), Space: 61.4 MB (53.14%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,35 +1,40 @@
 import random
-
 class RandomizedSet:
 
     def __init__(self):
-        self.appeared_to_index = {}
-        self.nums = []
+        self.vals = []
+        self.val_index_mapping = {}
+                
 
     def insert(self, val: int) -> bool:
-        if val in self.appeared_to_index:
+        if val in self.val_index_mapping:
             return False
         
-        self.nums.append(val)
-        self.appeared_to_index[val] = len(self.nums) - 1
+        self.vals.append(val)
+        self.val_index_mapping[val] = len(self.vals) - 1
+        
         return True
+        
 
     def remove(self, val: int) -> bool:
-        if val not in self.appeared_to_index:
+        if val not in self.val_index_mapping:
             return False
         
-        index = self.appeared_to_index[val]
-        last_val = self.nums[-1]
-        self.nums[index] = last_val
-        self.appeared_to_index[last_val] = index
-        self.appeared_to_index.pop(val)
-        self.nums.pop()
+        index = self.val_index_mapping[val]
+        last_val = self.vals[len(self.vals) - 1]
+        
+        self.vals[index] = last_val
+        self.val_index_mapping[last_val] = index
+        
+        self.val_index_mapping.pop(val)
+        self.vals.pop()
         
         return True
-        
+
     def getRandom(self) -> int:
-        random_idx = random.randint(0, len(self.nums) - 1)
-        return self.nums[random_idx]
+        size = len(self.vals)
+        index = random.randint(0, size - 1)
+        return self.vals[index]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
