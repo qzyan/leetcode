27ff3d18d54fade373b3b0c794173b2a71ca8f45
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2023-01-15 18:54:27 -0800

    Time: 80 ms (91.29%), Space: 17.7 MB (6.88%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,30 +1,35 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        left_idx = self.find_left_interval(intervals, newInterval)
-        right_idx = self.find_right_interval(intervals, newInterval)
+        if not intervals:
+            return [newInterval]
+        
+        # not no interval << newInterval, -1
+        left_idx = self.find_left_bound(intervals, newInterval)
+        # if no interval >> newInterval, len(intervals)
+        right_idx = self.find_right_bound(intervals, newInterval)
         
         if left_idx + 1 == right_idx:
-            intervals.insert(right_idx, newInterval)
-            return intervals
+            return intervals[:left_idx + 1] + [newInterval] + intervals[right_idx:]
         
-        return self.combine(intervals, left_idx, right_idx, newInterval)
+        return self.combine_overlap(intervals, newInterval, left_idx, right_idx)
         
-    def find_left_interval(self, intervals, newInterval):
+    def find_left_bound(self, intervals, newInterval):
         idx = 0
         while idx < len(intervals) and intervals[idx][1] < newInterval[0]:
             idx += 1
             
         return idx - 1
     
-    def find_right_interval(self, intervals, newInterval):
+    def find_right_bound(self, intervals, newInterval):
         idx = len(intervals) - 1
         while idx >= 0 and intervals[idx][0] > newInterval[1]:
             idx -= 1
-        
+            
         return idx + 1
     
-    def combine(self, intervals, left_idx, right_idx, newInterval):
-        new_start = min(intervals[left_idx + 1][0], newInterval[0])
-        new_end = max(intervals[right_idx - 1][1], newInterval[1])
-        return intervals[:left_idx + 1] + [[new_start, new_end]] + intervals[right_idx:]
+    def combine_overlap(self, intervals, newInterval, left_idx, right_idx):
+        combined_interval_start = min(intervals[left_idx + 1][0], newInterval[0])
+        combined_interval_end = max(intervals[right_idx - 1][1], newInterval[1])
+        combined_interval = [combined_interval_start, combined_interval_end]
         
+        return intervals[:left_idx + 1] + [combined_interval] + intervals[right_idx:]
