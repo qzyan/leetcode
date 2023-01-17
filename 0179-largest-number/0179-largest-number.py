2042dcad115081b8be7e0badf8953c28d9867169
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2023-01-16 19:05:23 -0800

    Time: 44 ms (72.32%), Space: 14 MB (20.08%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,6 +1,14 @@
+class NumberKey:
+    def __init__(self, x):
+        self.x = x
+        
+    def __lt__(self, other):
+        return str(self.x) + str(other.x) < str(other.x) + str(self.x)
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        sorted_nums = sorted(nums, key = cmp_to_key(self.comp), reverse = True)
+        sorted_nums = sorted(nums, key = NumberKey, reverse = True)
+        #sorted_nums = sorted(nums, key = cmp_to_key(self.comp), reverse = True)
         result = ''.join(map(str, sorted_nums))
         if result[0] == '0':
             return '0'
