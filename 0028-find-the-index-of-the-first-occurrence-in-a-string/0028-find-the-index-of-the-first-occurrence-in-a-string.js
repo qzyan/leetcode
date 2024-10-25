20b115e48d9831ad5d901fea3177df44ffe8972b
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2024-10-25 01:00:13 -0700

    Time: 44 ms (85.55%), Space: 55.7 MB (5.06%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,23 +3,62 @@
  * @param {string} needle
  * @return {number}
  */
+
+const HASHSIZE = 10 ** 6
+const MAGICNUM = 31
+    
 var strStr = function(haystack, needle) {
     if (!needle) {
         return 0
     }
-
+    
+    if (haystack.length < needle.length) {
+        return -1
+    }
+    
+    const needleHashVal = getHashValue(needle)
+    let hayStackHashVal = getHashValue(haystack.substring(0, needle.length))
+    let power = 1
+    for (let i = 0; i < needle.length; i++) {
+        power = (power * MAGICNUM) % HASHSIZE
+    }
+    
     for (let i = 0; i < haystack.length - needle.length + 1; i++) {
-        let isFound = true
-        for (let j = 0; j < needle.length; j++) {
-            if (haystack[i + j] != needle[j]) {
-                isFound = false
-                break
+        console.log(needleHashVal, hayStackHashVal)
+        if (hayStackHashVal === needleHashVal) {
+            if (compare(haystack, needle, i)) {
+                return i
             }
         }
-        if (isFound) {
-            return i
+        
+        hayStackHashVal = (hayStackHashVal * MAGICNUM + haystack.charCodeAt(i + needle.length)) % HASHSIZE
+        hayStackHashVal = hayStackHashVal - (haystack.charCodeAt(i) * power) % HASHSIZE
+        
+        if (hayStackHashVal < 0) {
+            hayStackHashVal += HASHSIZE
         }
     }
     
     return -1
+    
 };
+
+
+const getHashValue = (s) => {
+    let val = 0
+    for (let i = 0; i < s.length; i++) {
+        val = (val * MAGICNUM + s.charCodeAt(i)) % HASHSIZE
+    }
+    
+    return val
+}
+
+const compare = (haystack, needle, start_idx) => {
+    for (let i = 0; i < needle.length; i++) {
+        if (haystack[start_idx + i] !== needle[i]) {
+            return false
+        }
+    }
+    
+    return true
+}
