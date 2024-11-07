c5f62dd53a82ca889387447ad9104d96569dcf08
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2024-11-07 12:45:19 -0800

    Time: 24 ms (73.42%), Space: 13.3 MB (79.04%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,8 +1,15 @@
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        result = 0
-        for i in range(0, len(nums), 2):
-            result += nums[i]
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums);
+        
+        n = len(nums) // 2
+        
+        total = 0
+        for i in range(n):
+            total += nums[i * 2]
             
-        return result
+        return total
