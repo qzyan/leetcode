fbd61778d3f61ed15a1900836395137366669f1e
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-23 09:01:09 -1000

    LC-maximum-subarray-sum-with-one-deletion [Runtime: 35 ms; Memory: 26.1 MB]

--- dynamic-programming/maximum-subarray-sum-with-one-deletion.py
+++ dynamic-programming/maximum-subarray-sum-with-one-deletion.py
@@ -1,13 +1,13 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        dp = [[0] * len(arr) for i in range(2)]
-        dp[0][0] = arr[0]
-        dp[1][0] = 0
+        no_delete_curr_max = arr[0]
+        one_delete_curr_max = 0
         max_sum = arr[0]
 
         for i in range(1, len(arr)):
-            dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
-            dp[1][i] = max(dp[1][i - 1] + arr[i], dp[0][i - 1])
-            max_sum = max(max_sum, dp[0][i], dp[1][i])
+            no_delete_prev_max = no_delete_curr_max
+            no_delete_curr_max = max(no_delete_prev_max + arr[i], arr[i])
+            one_delete_curr_max = max(one_delete_curr_max + arr[i], no_delete_prev_max)
+            max_sum = max(max_sum, no_delete_curr_max, one_delete_curr_max)
 
         return max_sum
