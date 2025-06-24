999cbae94a8e7d4d9425577f665d0264f3ffa99b
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-23 21:16:35 -1000

    LC-coin-change [Runtime: 1291 ms; Memory: 18.2 MB]

--- breadth-first-search/coin-change.py
+++ breadth-first-search/coin-change.py
@@ -5,7 +5,7 @@ class Solution:
         dp[0] = 0
         for i in range(1, amount + 1):
             for coin in coin_set:
-                if dp[i - coin] != float("inf"):
+                if i - coin >= 0 and dp[i - coin] != float("inf"):
                     dp[i] = min(dp[i], dp[i - coin] + 1)
 
         return dp[amount] if dp[amount]!= float("inf") else -1
