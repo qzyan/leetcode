cd0a28d8facbb7c56a90cb60c39326d8a9da37c2
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-23 21:16:05 -1000

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- array/coin-change.py
+++ array/coin-change.py
@@ -1,24 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        coin_set = set(coins)
         dp = [float("inf")] * (amount + 1)
         dp[0] = 0
-        coin_set = set(coins)
-        for i in range(1, len(dp)):
-            if i in coin_set:
-                dp[i] = 1
-                continue
-
+        for i in range(1, amount + 1):
             for coin in coin_set:
-                if i - coin < 0:
-                    continue
+                if dp[i - coin] != float("inf"):
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
 
-                if dp[i - coin] == float("inf"):
-                    continue
-                
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-
-        return dp[amount] if dp[amount] != float("inf") else -1
-                
-
-                
+        return dp[amount] if dp[amount]!= float("inf") else -1
