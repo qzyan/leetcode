19851ca9b578cb9c338845e9dbfd594f52cf3e5d
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-24 19:35:19 -1000

    LC-minimum-path-sum [Runtime: 11 ms; Memory: 18.5 MB]

--- dynamic-programming/minimum-path-sum.py
+++ dynamic-programming/minimum-path-sum.py
@@ -1,15 +1,13 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [[float("inf")] * len(grid[0]) for _ in range(len(grid))]
-        dp[0][0] = grid[0][0]
+        dp = [0] * len(grid[0])
+        dp[0] = grid[0][0]
         for i in range(1, len(grid[0])):
-            dp[0][i] = dp[0][i - 1] + grid[0][i]
-        
-        for i in range(1, len(grid)):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
+            dp[i] = dp[i - 1] + grid[0][i]
 
         for row in range(1, len(grid)):
+            dp[0] = dp[0] + grid[row][0]
             for col in range(1, len(grid[0])):
-                dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + grid[row][col]
+                dp[col] = min(dp[col], dp[col - 1]) + grid[row][col]
 
-        return dp[-1][-1]
+        return dp[-1]
