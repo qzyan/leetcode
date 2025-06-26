b6a12bd8ea730e9b74c35cec0abda5041ed05215
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-25 21:06:34 -1000

    LC-interleaving-string [Runtime: N/A; Memory: N/A]

--- dynamic-programming/interleaving-string.py
+++ dynamic-programming/interleaving-string.py
@@ -5,20 +5,23 @@ class Solution:
         if not s2:
             return s1 == s3
         
-        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [[False] * (len(s2) + 1) for _ in range(2)]
         dp[0][0] = True
         for row in range(len(s1) + 1):
             for col in range(len(s2) + 1):
-                prev_state1 = dp[row - 1][col] if row - 1 >= 0 else False
-                prev_state2 = dp[row][col - 1] if col - 1 >= 0 else False
+                prev_state1 = dp[(row - 1) % 2][col] if row - 1 >= 0 else False
+                prev_state2 = dp[row % 2][col - 1] if col - 1 >= 0 else False
                 if not prev_state1 and not prev_state2:
                     continue
                 
                 if prev_state1 and s1[row - 1] == s3[row + col - 1]:
-                    dp[row][col] = True
+                    dp[row % 2][col] = True
                 
                 if prev_state2 and s2[col - 1] == s3[row + col - 1]:
-                    dp[row][col] = True
+                    dp[row % 2][col] = True
 
-        return dp[len(s1)][len(s2)]
+        return dp[len(s1) % 2][len(s2)]
 
