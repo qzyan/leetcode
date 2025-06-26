b6126499ed53bf835437197c3bae664eabfc3019
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-25 14:33:43 -1000

    LC-longest-increasing-subsequence [Runtime: 11 ms; Memory: 18.1 MB]

--- dynamic-programming/longest-increasing-subsequence.py
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -2,35 +2,32 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = []
         for num in nums:
-            if not dp:
+            if not dp or dp[-1] < num:
                 dp.append(num)
-                continue
-            if num > dp[-1]:
-                dp.append(num)
-                continue
-
-            idx = self.find_first_larger_or_equal(dp, num)
-            if dp[idx] == num:
                 continue
             
+            idx = self.first_larger_or_equal(dp, num)
             dp[idx] = num
 
         return len(dp)
-
-    def find_first_larger_or_equal(self, dp, target):
+    
+    def first_larger_or_equal(self, nums, target):
+        if target > nums[-1]:
+            return len(nums)
+        
         left = 0
-        right = len(dp) - 1
+        right = len(nums) - 1
         while left + 1 < right:
             mid = (left + right) // 2
-            if dp[mid] >= target:
+            if nums[mid] >= target:
                 right = mid
             else:
                 left = mid
 
-        if dp[left] >= target:
+        if nums[left] >= target:
             return left
         
-        if dp[right] >= target:
+        if nums[right] >= target:
             return right
         
-        return len(dp)
+        return len(nums)
