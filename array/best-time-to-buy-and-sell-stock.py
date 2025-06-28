2c99dcc6c737884822b96d2630018a20a8a481b6
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-28 09:34:35 -1000

    LC-best-time-to-buy-and-sell-stock [Runtime: 143 ms; Memory: 26.9 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-
-        curr_min = float("inf")
+        max_after_1st_purchase = -float("inf")
         max_profit = 0
+
         for price in prices:
-            if price < curr_min:
-                curr_min = price
-            else:
-                max_profit = max(max_profit, price - curr_min)
+            max_after_1st_purchase = max(max_after_1st_purchase, -price)
+            max_profit = max(max_profit, max_after_1st_purchase + price)
 
         return max_profit
-
