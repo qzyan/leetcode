4d87ab3a4e193a14c1665b1c54cd710d4aafbc53
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-28 09:46:09 -1000

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 3 ms; Memory: 18.7 MB]

--- greedy/best-time-to-buy-and-sell-stock-ii.py
+++ greedy/best-time-to-buy-and-sell-stock-ii.py
@@ -2,12 +2,12 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:
             return 0
-        
-        nohold = 0
-        hold = -prices[0]
-        for i in range(len(prices)):
-            price = prices[i]
-            hold = max(hold, nohold - price)
-            nohold = max(nohold, hold + price)
 
-        return max(hold, nohold)
+        hold = -prices[0]
+        nohold = 0
+
+        for i in range(1, len(prices)):
+            hold = max(hold, nohold - prices[i])
+            nohold = max(nohold, hold + prices[i])
+
+        return nohold
