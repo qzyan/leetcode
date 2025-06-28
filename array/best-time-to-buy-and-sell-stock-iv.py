f2be2efd2e6b76e921ede629499c98b642082f0a
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-27 15:29:20 -1000

    LC-best-time-to-buy-and-sell-stock-iv [Runtime: 51 ms; Memory: 18.8 MB]

--- array/best-time-to-buy-and-sell-stock-iv.py
+++ array/best-time-to-buy-and-sell-stock-iv.py
@@ -1,11 +1,16 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        buys = [-float("inf")] * (k + 1)
-        sells = [0] * (k + 1)
+        if k == 0:
+            return 0
+        if not prices:
+            return 0
 
-        for price in prices:
-            for i in range(1, k + 1):
-                buys[i] = max(buys[i], sells[i - 1] - price)
-                sells[i] = max(sells[i], buys[i] + price)
-
-        return sells[k]
+        dp = [[0] * len(prices) for _ in range(k + 1)]
+        for row in range(1, k + 1):
+            max_after_rowth_buy = -float("inf")
+            for col in range(len(prices)):
+                max_after_rowth_buy = max(max_after_rowth_buy, dp[row - 1][col] - prices[col])
+                if col - 1 >= 0:
+                    dp[row][col] = max(dp[row][col - 1], max_after_rowth_buy + prices[col])
+                    
+        return dp[k][len(prices) - 1]
