6f4502dc28d0b4ca1676ccc6ee74ff7472d90573
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-28 09:49:07 -1000

    LC-best-time-to-buy-and-sell-stock-iii [Runtime: 235 ms; Memory: 28.7 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock-iii.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock-iii.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
+        max_after_1st_buy = -float("inf")
+        max_after_1st_sell = 0
+        max_after_2nd_buy = -float("inf")
+        max_after_2nd_sell = 0
 
-        dp1 = [0] * len(prices) # max_profit after 1st trans
-        dp2 = [0] * len(prices) # max_profit after 2nd trans
+        for price in prices:
+            max_after_1st_buy = max(max_after_1st_buy, -price)
+            max_after_1st_sell = max(max_after_1st_sell, max_after_1st_buy + price)
+            max_after_2nd_buy = max(max_after_2nd_buy, max_after_1st_sell - price)
+            max_after_2nd_sell = max(max_after_2nd_sell, max_after_2nd_buy + price)
 
-        curr_min = prices[0]
-        max_left_afer_2nd_buy = -float("inf")
-        for idx, price in enumerate(prices):
-            dp1[idx] = max(dp1[idx - 1], price - curr_min)
-            if price < curr_min:
-                curr_min = price
-            
-            max_left_afer_2nd_buy = max(max_left_afer_2nd_buy, dp1[idx] - price)
-
-            dp2[idx] = max(dp2[idx - 1], max_left_afer_2nd_buy + price)
-
-        return dp2[-1]
+        return max_after_2nd_sell
