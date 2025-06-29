b248ebc759e439288065e05b1f57d6c36535d7ad
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-29 12:07:05 -1000

    LC-jump-game-ii [Runtime: 4751 ms; Memory: 18.4 MB]

---
+++ greedy/jump-game-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = [float("inf")] * len(nums)
+        dp[0] = 0
+        for idx in range(len(nums)):
+            if dp[idx] == float("inf"):
+                continue
+            
+            for step in range(1, nums[idx] + 1):
+                if step + idx >= len(nums):
+                    break
+                
+                dp[step + idx] = min(dp[step + idx], dp[idx] + 1)
+
+        return dp[-1]
