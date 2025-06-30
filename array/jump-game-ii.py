3e08e4b53d26837e6caf3b17114f5e54a31c0cf4
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-29 14:46:52 -1000

    LC-jump-game-ii [Runtime: 11 ms; Memory: 18.6 MB]

--- array/jump-game-ii.py
+++ array/jump-game-ii.py
@@ -1,16 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         curr_idx = 0
-        furthest_idx = 0
+        curr_furthest_idx = 0
+        next_furthest_idx = 0
         step = 0
-        while furthest_idx < len(nums) - 1:
-            new_furthest_idx = furthest_idx
-            
-            while curr_idx <= furthest_idx:
-                new_furthest_idx = max(new_furthest_idx, curr_idx + nums[curr_idx])
-                curr_idx += 1
-            
-            step += 1
-            furthest_idx = new_furthest_idx
-        
+        while curr_idx < len(nums):
+            if curr_furthest_idx >= len(nums) - 1:
+                break
+
+            next_furthest_idx = max(next_furthest_idx, nums[curr_idx] + curr_idx)
+            if curr_idx == curr_furthest_idx:
+                step += 1
+                curr_furthest_idx = next_furthest_idx
+
+            curr_idx += 1
+
         return step
