1fc4048bbb457e433757f2c322ed2bce18fbdb92
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-06-29 14:58:27 -1000

    LC-jump-game-ii [Runtime: 17 ms; Memory: 19 MB]

--- dynamic-programming/jump-game-ii.py
+++ dynamic-programming/jump-game-ii.py
@@ -8,13 +8,14 @@ class Solution:
             end_idx = furthest_idx
             for _ in range(len(queue)):
                 curr_idx = queue.popleft()
+                if curr_idx == len(nums) - 1:
+                    return step
+
                 furthest_idx = max(furthest_idx, curr_idx + nums[curr_idx])
 
             step += 1
-            if furthest_idx >= len(nums) - 1:
-                break
 
-            for idx in range(end_idx, furthest_idx + 1):
+            for idx in range(end_idx + 1, min(furthest_idx + 1, len(nums))):
                 queue.append(idx)
 
         return step
