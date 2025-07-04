9ab46a01755b82c24f279649261744c803cfa87c
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-04 09:11:37 -1000

    LC-maximum-difference-score-in-a-grid [Runtime: 272 ms; Memory: 30.5 MB]

--- array/maximum-difference-score-in-a-grid.py
+++ array/maximum-difference-score-in-a-grid.py
@@ -1,23 +1,11 @@
 class Solution:
     def maxScore(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        
-        dp = [[-float("inf")] * len(grid[0]) for _ in range(len(grid))]
+        dp = [[float("inf")] * len(grid[0]) for _ in range(len(grid))]
         res = -float("inf")
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                for j in range(row):
-                    diff = grid[row][col] - grid[j][col]
-                    score = max(0, dp[j][col]) + diff
-                    dp[row][col] = max(dp[row][col], score)
-                
-                for k in range(col):
-                    diff = grid[row][col] - grid[row][k]
-                    score = max(0, dp[row][k]) + diff
-                    dp[row][col] = max(dp[row][col], score)
-
-                res = max(res, dp[row][col])
+                prev_min = min(dp[row - 1][col] if row else float("inf"), dp[row][col - 1] if col else float("inf"))
+                dp[row][col] = min(grid[row][col], prev_min) 
+                res = max(res, grid[row][col] - prev_min)
 
         return res
-                
