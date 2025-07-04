19318089e008f086eff4ef405344452ccd99f4cc
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-03 21:03:08 -1000

    LC-find-the-maximum-length-of-a-good-subsequence-ii [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-the-maximum-length-of-a-good-subsequence-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+        dp = [{} for _ in range(k + 1)]
+        prev_res = [0] * (k + 1)
+        res = [0] * (k + 1)
+
+        for num in nums:
+            for i in range(k + 1):
+                dp[i][num] = dp[i].get(num, 0) + 1
+                dp[i][num] = max(dp[i][num], (prev_res[i - 1] + 1 if i else 1))
+                res[i] = max(res[i], dp[i][num])
+            
+            prev_res = res
+            res = [0] * (k + 1)
+        
+        return prev_res[k]
+
+
