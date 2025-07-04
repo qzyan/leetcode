34cfa92226f715b19fbdbe840d423948a7314470
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-03 17:48:05 -1000

    LC-find-the-maximum-length-of-a-good-subsequence-i [Runtime: 59 ms; Memory: 18 MB]

--- dynamic-programming/find-the-maximum-length-of-a-good-subsequence-i.py
+++ dynamic-programming/find-the-maximum-length-of-a-good-subsequence-i.py
@@ -1,14 +1,17 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return 0
+        dp1 = [{} for _ in range(k + 1)]
+        dp2 = [[0] * len(nums) for _ in range(k + 1)]
 
-        dp = [{} for _ in range(k + 1)]
-        res = [0] * (k + 1)
+        for col in range(len(nums)):
+            num = nums[col]
+            dp1[0][num] = dp1[0].get(num, 0) + 1
+            dp2[0][col] = max(dp2[0][col - 1], dp1[0][num]) if col - 1 >= 0 else dp1[0][num]
 
-        for num in nums:
-            for i in range(k, -1, -1):
-                dp[i][num] = max(dp[i][num] + 1 if num in dp[i] else 1, res[i - 1] + 1 if i else 0)
-                res[i] = max(res[i], dp[i][num])
+        for row in range(1, k + 1):
+            for col in range(1, len(nums)):
+                num = nums[col]
+                dp1[row][num] = max(dp1[row].get(num, 0) + 1, dp2[row - 1][col - 1] + 1)
+                dp2[row][col] = max(dp2[row][col - 1], dp1[row][num]) if col - 1 >= 0 else dp1[row][num]
 
-        return res[k]
+        return max([dp2[row][-1] for row in range(k + 1)])
