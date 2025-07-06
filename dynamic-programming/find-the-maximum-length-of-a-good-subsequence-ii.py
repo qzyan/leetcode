701dd9a4834d30dad3bc1f2bbe483b6165e8a511
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-06 09:04:52 -1000

    LC-find-the-maximum-length-of-a-good-subsequence-ii [Runtime: 1034 ms; Memory: 25.9 MB]

--- dynamic-programming/find-the-maximum-length-of-a-good-subsequence-ii.py
+++ dynamic-programming/find-the-maximum-length-of-a-good-subsequence-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
         dp = [{} for _ in range(k + 1)]
-        prev_res = [0] * (k + 1)
         res = [0] * (k + 1)
 
         for num in nums:
             for i in range(k + 1):
-                dp[i][num] = dp[i].get(num, 0) + 1
-                dp[i][num] = max(dp[i][num], (prev_res[i - 1] + 1 if i else 1))
-                res[i] = max(prev_res[i], dp[i][num])
+                if num not in dp[i]:
+                    dp[i][num] = res[i - 1] + 1 if i else 1
+                else:
+                    dp[i][num] = max(dp[i][num], res[i - 1] if i else 0) + 1
+
+            for j in range(k + 1):
+                res[j] = max(res[j], dp[j][num])
             
-            prev_res = res
-            res = [0] * (k + 1)
-        
-        return prev_res[k]
+            
 
-
+        return res[k]
