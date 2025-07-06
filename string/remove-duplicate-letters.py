40f22a420d614a7dcaafa6aa5121e3c2536a46cf
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-05 16:02:10 -1000

    LC-remove-duplicate-letters [Runtime: 0 ms; Memory: 17.8 MB]

--- string/remove-duplicate-letters.py
+++ string/remove-duplicate-letters.py
@@ -1,28 +1,38 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        visited = set()
         stack = []
-        char_last_idx_mapping = self.get_char_last_idx_mapping(s)
+        visited = set()
+        char_last_idx = self.get_last_idx(s)
         for idx in range(len(s)):
             char = s[idx]
             if char in visited:
                 continue
-            
-            while stack and s[stack[-1]] > char and self.is_existing_after(s[stack[-1]], idx, char_last_idx_mapping):
-                visited.remove(s[stack.pop()])
+
+            while stack:
+                last_char_in_stack = s[stack[-1]]
+                if last_char_in_stack > char and self.exists_after(last_char_in_stack, idx, char_last_idx):
+                    stack.pop()
+                    visited.remove(last_char_in_stack)
+                else:
+                    break
             
             stack.append(idx)
             visited.add(char)
 
-        return "".join([ s[i] for i in stack])
+        return "".join([s[idx] for idx in stack])
 
-    def is_existing_after(self, char, idx, char_last_idx_mapping):
-        return char_last_idx_mapping[char] > idx
+    def get_last_idx(self, s):
+        results = {}
+        for idx in range(len(s) - 1, -1, -1):
+            char = s[idx]
+            if char in results:
+                continue
 
-    def get_char_last_idx_mapping(self, s):
-        mapping = {}
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] not in mapping:
-                mapping[s[i]] = i
+            results[char] = idx
 
-        return mapping
+        return results
+
+    def exists_after(self, char, idx, char_last_idx):
+        return char_last_idx[char] > idx
+
+    
