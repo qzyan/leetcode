a5aba06e3835b2f20af0b130c45fa5e7fdc038cd
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-10 16:49:17 -1000

    LC-contains-duplicate-ii [Runtime: 27 ms; Memory: 37.2 MB]

--- array/contains-duplicate-ii.py
+++ array/contains-duplicate-ii.py
@@ -1,26 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        if not nums or len(nums) == 1:
+        if not nums:
             return False
-        if k == 0:
-            return False
-        if nums[0] == nums[1]:
-            return True
-
-        slow = 0
-        fast = 1
-        uniques = set([nums[0]])
-        while fast < len(nums):
-            if fast - slow > k:
-                uniques.remove(nums[slow])
-                slow += 1
+        num_idx_mapping = {}
+        for idx, num in enumerate(nums):
+            if num not in num_idx_mapping:
+                num_idx_mapping[num] = idx
                 continue
             
-            if nums[fast] in uniques:
+            if idx - num_idx_mapping[num] <= k:
                 return True
-            
-            uniques.add(nums[fast])
-            fast += 1
-        
-        return False
+            else:
+                num_idx_mapping[num] = idx
 
+        return False
