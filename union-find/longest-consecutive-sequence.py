8f9e45ea11bebb42a0fc967c1e3354632bb20815
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-10 16:59:30 -1000

    LC-longest-consecutive-sequence [Runtime: 46 ms; Memory: 34.2 MB]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -1,17 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
         nums_set = set(nums)
-        res = 1
+        res = 0
         for num in nums_set:
             if num - 1 in nums_set:
                 continue
             
-            i = num + 1
+            i = num
             while i in nums_set:
-                res = max(res, i - num + 1)
                 i += 1
+            
+            res = max(res, i - num)
 
         return res
