87b4ace410b9737daae4be575d2e8fcbaa5ad014
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-10 16:21:22 -1000

    LC-4sum [Runtime: 135 ms; Memory: 18.1 MB]

--- sorting/4sum.py
+++ sorting/4sum.py
@@ -1,44 +1,45 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        if not nums or len(nums) < 4:
+        if not nums:
             return []
 
         nums = sorted(nums)
-        return self.k_sum(nums, 0, 4, target)
+        return self.k_sum(nums, 4, 0, target)
 
-    def k_sum(self, nums, start_idx, k, target):
+    def k_sum(self, nums, k, start_idx, target):
         if k == 2:
             return self.two_sum(nums, start_idx, target)
-
         results = []
         for idx in range(start_idx, len(nums) - k + 1):
-            if idx != start_idx and nums[idx] == nums[idx - 1]:
+            num = nums[idx]
+            if num * k > target:
+                break
+
+            if idx > start_idx and nums[idx] == nums[idx - 1]:
                 continue
             
-            if nums[idx] * k > target:
-                break
-            
-            sub_results = self.k_sum(nums, idx + 1, k - 1, target - nums[idx])
-            for sub_result in sub_results:
-                result = ([nums[idx]] + sub_result) 
-                results.append(result)
+            sub_results = self.k_sum(nums, k - 1, idx + 1, target - num)
+            for sub_res in sub_results:
+                results.append([num] + sub_res)
 
         return results
 
     def two_sum(self, nums, start_idx, target):
-        left = start_idx
-        right = len(nums) - 1
+        left, right = start_idx, len(nums) - 1
         results = []
         while left < right:
             total = nums[left] + nums[right]
-            if total == target:
+            if total < target:
+                left += 1
+            elif total > target:
+                right -= 1
+            else:
                 results.append([nums[left], nums[right]])
                 left += 1
                 while left < right and nums[left] == nums[left - 1]:
                     left += 1
-            elif total < target:
-                left += 1
-            else:
-                right -= 1
-
+        
         return results
+
+            
+
