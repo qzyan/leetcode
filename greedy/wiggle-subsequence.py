b86d62432c125cc69094fac036400468014f9d23
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-14 13:26:09 -1000

    LC-wiggle-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/wiggle-subsequence.py
+++ greedy/wiggle-subsequence.py
@@ -2,23 +2,23 @@ class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
-        dp = [1] * len(nums)
-        is_pos = [False] * len(nums)
 
-        max_len = 1
-        for idx in range(1, len(nums)):
-            if nums[idx] != nums[0]:
-                dp[idx] = 2
-                if nums[idx] > nums[0]:
-                    is_pos[idx] = True
+        dp = [nums[0]]
+        is_diff_pos = False
+        for i in range(1, len(nums)):
+            num = nums[i]
+            if num == dp[-1]:
+                continue
+            
+            if num > dp[-1] and is_diff_pos:
+                dp[-1] = num
+            elif num > dp[-1] and not is_diff_pos:
+                dp.append(num)
+                is_diff_pos = True
+            elif num < dp[-1] and is_diff_pos:
+                dp.append(num)
+                is_diff_pos = False
+            else:
+                dp[-1] = num
 
-            for j in range(1, idx):
-                if (is_pos[j] and nums[idx] < nums[j]) or (not is_pos[j] and nums[idx] > nums[j]):
-                    if dp[idx] < dp[j] + 1:
-                        dp[idx] = dp[j] + 1
-                        is_pos[idx] = nums[idx] > nums[j]
-
-            max_len = max(max_len, dp[idx])
-
-        return max_len
+        return len(dp)
