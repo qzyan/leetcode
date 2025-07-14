a1974d124ad9f9efde1e20d4481733e64544b63c
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-14 13:01:11 -1000

    LC-wiggle-subsequence [Runtime: N/A; Memory: N/A]

---
+++ array/wiggle-subsequence.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        if len(nums) <= 2:
+            return 2
+
+        dp = [1] * len(nums)
+        is_pos = [False] * len(nums)
+
+        max_len = 1
+        for idx in range(1, len(nums)):
+            dp[idx] = 2
+            if nums[idx] > nums[0]:
+                is_pos[idx] = True
+
+            for j in range(1, idx):
+                if (is_pos[j] and nums[idx] < nums[j]) or (not is_pos[j] and nums[idx] > nums[j]):
+                    if dp[idx] < dp[j] + 1:
+                        dp[idx] = dp[j] + 1
+                        is_pos[idx] = nums[idx] > nums[j]
+
+            max_len = max(max_len, dp[idx])
+
+        return max_len
