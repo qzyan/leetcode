aca615e24e5c31e8abf54ab634a57c7e8d36db57
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-13 20:47:54 -1000

    LC-minimum-remove-to-make-valid-parentheses [Runtime: 31 ms; Memory: 19.4 MB]

--- string/minimum-remove-to-make-valid-parentheses.py
+++ string/minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        res = []
-        fp_count = 0
-        bp_count = 0
-        for char in s:
-            if char not in ("(", ")"):
-                res.append(char)
+        stack = []
+        chars = list(s)
+        for i, char in enumerate(chars):
             if char == "(":
-                res.append("(")
-                fp_count += 1
-            if char == ")" and fp_count > bp_count:
-                res.append(")")
-                bp_count += 1
-
-        for i in range(len(res) - 1, -1, -1):
-            char = res[i]
-            if char == "(" and fp_count > bp_count:
-                fp_count -= 1
-                res[i] = ""
-
-        return "".join(res)
+                stack.append(i)
             
+            if char == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    chars[i] = ""
 
+        for idx in stack:
+            chars[idx] = ""
 
-        
+        return "".join(chars)
