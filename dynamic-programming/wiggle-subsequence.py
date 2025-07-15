3a080cf5f806fecd6717b4757be6ba28fb92fdb0
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-14 16:11:14 -1000

    LC-wiggle-subsequence [Runtime: 0 ms; Memory: 17.8 MB]

--- dynamic-programming/wiggle-subsequence.py
+++ dynamic-programming/wiggle-subsequence.py
@@ -4,30 +4,20 @@ class Solution:
             return 0
 
         dp = [nums[0]]
-        is_up = 0
-
         for i in range(1, len(nums)):
             num = nums[i]
             if num == dp[-1]:
                 continue
-            
-            if is_up == 0:
-                is_up = num - dp[-1]
+
+            if len(dp) == 1:
                 dp.append(num)
-            elif is_up > 0:
-                if num > dp[-1]:
-                    dp[-1] = num
-                elif num < dp[-1]:
-                    is_up = num - dp[-1]
-                    dp.append(num)
-            elif is_up < 0:
-                if num > dp[-1]:
-                    is_up = num - dp[-1]
-                    dp.append(num)
-                elif num < dp[-1]:
-                    dp[-1] = num
+            elif num > dp[-1] and dp[-1] > dp[-2]:
+                dp[-1] = num
+            elif num > dp[-1] and dp[-1] < dp[-2]:
+                dp.append(num)
+            elif num < dp[-1] and dp[-1] > dp[-2]:
+                dp.append(num)
+            elif num < dp[-1] and dp[-1] < dp[-2]:
+                dp[-1] = num
 
         return len(dp)
-
-
-        
