23a287b68e3d47f62ab268f36b092250eac17b53
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-19 10:10:23 -1000

    LC-number-of-orders-in-the-backlog [Runtime: 712 ms; Memory: 56.8 MB]

--- simulation/number-of-orders-in-the-backlog.py
+++ simulation/number-of-orders-in-the-backlog.py
@@ -1,48 +1,47 @@
-MOD = 10 ** 9 + 7
-
-class OrderType:
-    SELL = 1
+M = 10 ** 9 + 7
+class TradeType:
     BUY = 0
-
+    SELL = 1
 class Solution:
     def getNumberOfBacklogOrders(self, orders: List[List[int]]) -> int:
         if not orders:
             return 0
-
-        buys_maxheap = []
-        sells_minheap = []
-
+        buy_max_heap = [] # [[-50, 5], [-40, 3], ....] get the highest buy order
+        sell_min_heap = [] # [[30, 5], [60, 3], ....] get the lowest sell order
         for order in orders:
-            price, qty, order_type = order
-            if order_type == OrderType.SELL:
-                heapq.heappush(sells_minheap, (price, qty))
+            p, qty, order_type = order
+            if order_type == TradeType.BUY:
+                heapq.heappush(buy_max_heap, (-p, qty))
+            else:
+                heapq.heappush(sell_min_heap, (p, qty))
 
-            if order_type == OrderType.BUY:
-                heapq.heappush(buys_maxheap, (-price, qty))
-
-            while buys_maxheap and sells_minheap and -buys_maxheap[0][0] >= sells_minheap[0][0]:
-                buy_price, buy_qty = heapq.heappop(buys_maxheap)
-                buy_price = -buy_price
-                sell_price, sell_qty = heapq.heappop(sells_minheap)
-
-                trade = min(sell_qty, buy_qty)
-
-                if buy_qty - trade > 0:
-                    heapq.heappush(buys_maxheap, (-buy_price, buy_qty - trade))
+            while buy_max_heap and sell_min_heap:
+                buy_max_p = -buy_max_heap[0][0]
+                sell_min_p = sell_min_heap[0][0]
+                if buy_max_p < sell_min_p:
+                    break
                 
-                if sell_qty - trade > 0:
-                    heapq.heappush(sells_minheap, (sell_price, sell_qty - trade))
+                b_price, b_qty = heapq.heappop(buy_max_heap)
+                b_price = -b_price
+                s_price, s_qty = heapq.heappop(sell_min_heap)
 
+                deal_qty = min(b_qty, s_qty)
+
+                if b_qty - deal_qty > 0:
+                    heapq.heappush(buy_max_heap, (-b_price, b_qty - deal_qty))
+
+                if s_qty - deal_qty > 0:
+                    heapq.heappush(sell_min_heap, (s_price, s_qty - deal_qty))
+
+        res = self.get_count(buy_max_heap) + self.get_count(sell_min_heap)
+        return res % M
         
-        res = 0
-        for order in buys_maxheap:
-            res += order[1] 
-            res %= MOD
+    def get_count(self, orders):
+        result = 0
+        for order in orders:
+            _, qty = order
+            result += qty
+            result % M
+        return result
 
-        for order in sells_minheap:
-            res += order[1]
-            res %= MOD
-
-        return res
-
-        
+    
