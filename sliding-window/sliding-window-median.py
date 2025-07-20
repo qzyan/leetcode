610bdfa818757a7fb1bbf53b99b4bf7f6a600821
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-19 21:44:28 -1000

    LC-sliding-window-median [Runtime: N/A; Memory: N/A]

--- sliding-window/sliding-window-median.py
+++ sliding-window/sliding-window-median.py
@@ -1,71 +1,49 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        results = []
-        sortedlist = SortedList()
-        left_idx = 0
-        idx = 0
-        nums_in_window = []
-        while idx < k:
-            num = nums[idx]
-            sortedlist.add(num)
-            idx += 1
+        small_max_heap = [] # get the largest in 1st half
+        large_min_heap = [] # get the smallest in 2nd half
 
-        m = (sortedlist[len(sortedlist) // 2] + sortedlist[(len(sortedlist) - 1) // 2]) / 2
-        results.append(m)
-        while idx < len(nums):
-            num = nums[idx]
-            sortedlist.add(num)
+        for i in range(k):
+            heapq.heappush(small_max_heap, (-nums[i], i))
 
-            left_num = nums[left_idx]
-            sortedlist.remove(left_num)
+        for _ in range(k // 2):
+            self.move(small_max_heap, large_min_heap)
 
-            m = (sortedlist[len(sortedlist) // 2] + sortedlist[(len(sortedlist) - 1) // 2]) / 2
-            results.append(m)
+        results = [self.get_median(small_max_heap, large_min_heap, k)]
 
-            left_idx += 1
-            idx += 1
+        for i in range(k, len(nums)):
+            idx_remove = i - k
+            if not small_max_heap or nums[i] <= -small_max_heap[0][0]:
+                heapq.heappush(small_max_heap, (-nums[i], i))
+                if large_min_heap and nums[idx_remove] > -small_max_heap[0][0]:
+                    self.move(small_max_heap, large_min_heap)
+            else:
+                heapq.heappush(large_min_heap, (nums[i], i))
+                if small_max_heap and nums[idx_remove] <= -small_max_heap[0][0]:
+                    self.move(large_min_heap, small_max_heap)
+
+            while small_max_heap and small_max_heap[0][1] <= i - k:
+                heapq.heappop(small_max_heap)
+            while large_min_heap and large_min_heap[0][1] <= i - k:
+                heapq.heappop(large_min_heap)
+
+            results.append(self.get_median(small_max_heap, large_min_heap, k))
 
         return results
 
-    def insert(self, num, max_heap, min_heap):
-        if not max_heap:
-            heapq.heappush(max_heap, -num)
-            return
+    def move(self, from_heap, to_heap):
+        num, idx = heapq.heappop(from_heap)
+        heapq.heappush(to_heap, (-num, idx))
 
-        if num <= -max_heap[0]:
-            heapq.heappush(max_heap, -num)
-        else:
-            heapq.heappush(min_heap, num)
+    def get_median(self, small, large, k):
+        if k % 2 == 1:
+            num, idx = small[0]
+            return -num
 
-        if len(max_heap) - 1 > len(min_heap):
-            num = heapq.heappop(max_heap)
-            num = -num
-            heapq.heappush(min_heap, num)
-        elif len(max_heap) < len(min_heap):
-            num = heapq.heappop(min_heap)
-            heapq.heappush(max_heap, -num)
+        num1, idx = small[0]
+        num2, idx = large[0]
 
-    def remove(self, num, max_heap, min_heap):
-        if not max_heap:
-            heapq.heappush(max_heap, -num)
-            return
-
-        if num <= -max_heap[0]:
-            heapq.heappush(max_heap, -num)
-        else:
-            heapq.heappush(min_heap, num)
-
-        if len(max_heap) - 1 > len(min_heap):
-            num = heapq.heappop(max_heap)
-            num = -num
-            heapq.heappush(min_heap, num)
-        elif len(max_heap) < len(min_heap):
-            num = heapq.heappop(min_heap)
-            heapq.heappush(max_heap, -num)
-    
-    def get_median(self, max_heap, min_heap):
-        if len(max_heap) == len(min_heap):
-            return (heapq.heappop(min_heap) - heapq.heappop(max_heap)) / 2
+        return (num2 - num1) / 2
         
-        return float(-heapq.heappop(max_heap))
-        
+            
+            
