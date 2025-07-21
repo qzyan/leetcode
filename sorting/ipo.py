ca7dcc3245490cc443149ca45f51ef2e3783309f
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-20 19:47:29 -1000

    LC-ipo [Runtime: N/A; Memory: N/A]

---
+++ sorting/ipo.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        max_profits = []
+        capital = capital[:]
+        while k > 0:
+            idxs = self.find_available_projects(capital, w)
+            for idx in idxs:
+                profit = profits[idx]
+                heapq.heappush(max_profits, -profit)
+
+            w -= heapq.heappop(max_profits)
+            k -= 1
+        
+        return w
+
+    def find_available_projects(self, capital, w):
+        idxs = []
+        for idx, c in enumerate(capital):
+            if c <= w:
+                idxs.append(idx)
+                # mark it done
+                capital[idx] = float("inf")
+
+        return idxs
