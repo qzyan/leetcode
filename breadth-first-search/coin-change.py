05ec31c16fd7120088de5150202b4449173b0464
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-20 21:40:50 -1000

    LC-coin-change [Runtime: 806 ms; Memory: 18.2 MB]

--- breadth-first-search/coin-change.py
+++ breadth-first-search/coin-change.py
@@ -1,25 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
         coins = sorted(coins)
-        memo = {}
-        return self.helper(coins, amount, memo)
 
-    def helper(self, coins, amount, memo):
-        if amount == 0:
-            return 0
+        for idx in range(1, amount + 1):
+            prev_res = float("inf")
+            for coin in coins:
+                if idx - coin >= 0:
+                    prev_res = min(dp[idx - coin], prev_res)
+            
+            if prev_res != float("inf"):
+                dp[idx] = prev_res + 1
 
-        if amount in memo:
-            return memo[amount]
-        min_subres = float("inf")
-        for coin in coins:
-            if coin > amount:
-                break
-            sub_res = self.helper(coins, amount - coin, memo)
-            if sub_res != -1:
-                min_subres = min(min_subres, sub_res)
-        
-        res = -1 if min_subres == float("inf") else min_subres + 1
-        memo[amount] = res
-        return res
+        return dp[-1] if dp[-1] != float("inf") else -1
