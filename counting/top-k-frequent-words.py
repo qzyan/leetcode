5642650b49cd940033a106c02168ca8ace51739a
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-21 21:48:21 -1000

    LC-top-k-frequent-words [Runtime: 2 ms; Memory: 17.9 MB]

--- counting/top-k-frequent-words.py
+++ counting/top-k-frequent-words.py
@@ -1,9 +1,26 @@
+class Element:
+    def __init__(self, count, word):
+        self.count = count
+        self.word = word
+
+    def __lt__(self, other):
+        if self.count != other.count:
+            return self.count < other.count
+
+        return self.word > other.word
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        if not words:
-            return []
-        word_count = Counter(words)
+        heap = [] # keep the largest elements, remove smaller
+        word_count_map = Counter(words)
 
-        results = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in word_count_map.items():
+            heapq.heappush(heap, Element(count, word))
+            if len(heap) > k:
+                heapq.heappop(heap)
 
-        return [word for word, _ in results[:k]]
+        sorted_elements = sorted(heap, key=lambda ele: (-ele.count, ele.word))
+        return [ele.word for ele in sorted_elements]
+
+
