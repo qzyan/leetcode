a475ea74fcb9f41674c0457b689f70dd2fb94982
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-21 21:30:14 -1000

    LC-top-k-frequent-words [Runtime: 3 ms; Memory: 17.8 MB]

--- array/top-k-frequent-words.py
+++ array/top-k-frequent-words.py
@@ -2,20 +2,8 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         if not words:
             return []
+        word_count = Counter(words)
 
-        word_count = self.count_words(words)
-        
-        # [("word", 5)]
-        items = list(word_count.items())
-        items.sort(key=lambda x: (-x[1], x[0]))
+        results = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
 
-        return [item[0] for item in items[:k]]
-
-    def count_words(self, words):
-        mapping = {}
-        for word in words:
-            mapping[word] = mapping.get(word, 0) + 1
-
-        return mapping
-
-        
+        return [word for word, _ in results[:k]]
