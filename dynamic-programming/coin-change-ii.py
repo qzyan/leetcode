fce42e63d0616b981a37349a0a760ab5ca1f130e
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-23 14:01:01 -1000

    LC-coin-change-ii [Runtime: 343 ms; Memory: 61.8 MB]

--- dynamic-programming/coin-change-ii.py
+++ dynamic-programming/coin-change-ii.py
@@ -1,9 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
-        for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] += dp[i - coin]
+        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
+        for row in range(len(dp)):
+            dp[row][0] = 1
 
-        return dp[-1]
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[0])):
+                coin = coins[row - 1]
+                dp[row][col] = dp[row - 1][col] + (dp[row][col - coin] if col - coin >= 0 else 0)
+
+        return dp[-1][-1]
