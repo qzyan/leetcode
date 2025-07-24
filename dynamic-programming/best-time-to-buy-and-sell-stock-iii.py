085054d1937ead8f09357b1c9d5e6c96fd46d21f
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-23 15:14:03 -1000

    LC-best-time-to-buy-and-sell-stock-iii [Runtime: 297 ms; Memory: 32.9 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock-iii.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock-iii.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp1 = [0] * len(prices) # util idx day, with n     trade, max profit can get
-        dp2 = [0] * len(prices) # util idx day, with n + 1 trade, max profit can get
-        
-        for _ in range(2):
+        if not prices:
+            return 0
+
+        n = len(prices)
+        dp = [[0] * n for _ in range(3)]
+        for k in range(1, 3):
             max_after_buy = -float("inf")
-            for idx, price in enumerate(prices):
-                max_after_buy = max(max_after_buy, dp1[idx] - price)
-                dp2[idx] = max(dp2[idx - 1], max_after_buy + price) if idx - 1 >= 0 else 0
+            for i in range(n):
+                max_after_buy = max(max_after_buy, dp[k - 1][i] - prices[i])
+                dp[k][i] = max(dp[k][i - 1], max_after_buy + prices[i])
 
-            dp1 = dp2
-            dp2 = [0] * len(prices)
-
-        return dp1[-1]
+        return dp[-1][-1]
