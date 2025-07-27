34c0a500be6b9584da5625cb8b405fcdbbe1563b
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-26 19:21:25 -1000

    LC-shortest-subarray-with-sum-at-least-k [Runtime: 165 ms; Memory: 34 MB]

--- sliding-window/shortest-subarray-with-sum-at-least-k.py
+++ sliding-window/shortest-subarray-with-sum-at-least-k.py
@@ -1,21 +1,19 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        prefix_sums = [0] * (len(nums) + 1)
-        sum_val = 0
-        for idx, num in enumerate(nums):
-            sum_val += num
-            prefix_sums[idx + 1] = sum_val
+        prefix_sums = [0]
+        for num in nums:
+            curr_sum = num + prefix_sums[-1]
+            prefix_sums.append(curr_sum)
 
-        queue = deque() # mono queue, [1,3,5, 10] prefix[idx] inc
-        length = float("inf")
+        queue = collections.deque()
+        min_len = float("inf")
         for idx in range(len(prefix_sums)):
-            prefix_sum = prefix_sums[idx]
-            while queue and prefix_sums[queue[-1]] >= prefix_sum:
+            curr_prefix_sum = prefix_sums[idx]
+            while queue and prefix_sums[queue[-1]] >= curr_prefix_sum:
                 queue.pop()
 
             queue.append(idx)
-            while queue and prefix_sum - prefix_sums[queue[0]] >= k:
-                length = min(length, idx - queue.popleft())
+            while queue and curr_prefix_sum - prefix_sums[queue[0]] >= k:
+                min_len = min(min_len, idx - queue.popleft())
 
-        return length if length != float("inf") else -1
-        
+        return min_len if min_len != float("inf") else -1
