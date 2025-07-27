036d5e8e05e0c73cf50c752555f9422bea5eb879
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-26 16:03:57 -1000

    LC-minimum-window-substring [Runtime: 75 ms; Memory: 18.1 MB]

--- sliding-window/minimum-window-substring.py
+++ sliding-window/minimum-window-substring.py
@@ -2,49 +2,46 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if not s or not t:
             return ""
-        
-        t_set = set(t)
-        t_char_count_mapping = self.get_char_count(t)
-        s_valid_char_count_mapping = {}
-        valid_char_in_s_count = 0
-        slow, fast = 0, 0
+
+        t_count = Counter(t)
+        s_count = defaultdict(int)
+        included_char_count = 0
+        left, right = 0, 0
         min_len = float("inf")
-        l, r = 0, 0
-        while fast < len(s):
-            char = s[fast]
-            if char not in t_set:
-                fast += 1
-                continue
-            
-            s_valid_char_count_mapping[char] = s_valid_char_count_mapping.get(char, 0) + 1
-            if s_valid_char_count_mapping[char] == t_char_count_mapping[char]:
-                valid_char_in_s_count += 1
-            
-            if valid_char_in_s_count < len(t_set):
-                fast += 1
+        result_l, result_r = 0, 0
+        while right < len(s):
+            char = s[right]
+            right += 1
+            # not related
+            if char not in t_count:
                 continue
 
-            while s[slow] not in t_set or s_valid_char_count_mapping[s[slow]] > t_char_count_mapping[s[slow]]:
-                if s[slow] not in t_set:
-                    slow += 1
+            s_count[char] += 1
+            if s_count[char] == t_count[char]:
+                included_char_count += 1
+
+            if included_char_count < len(t_count):
+                continue
+
+            while left < right:
+                left_char = s[left]
+                if left_char not in t_count:
+                    left += 1
                     continue
+                                
+                if right - left < min_len:
+                    min_len = right - left
+                    result_l = left
+                    result_r = right
                 
-                s_valid_char_count_mapping[s[slow]] -= 1
-                slow += 1
-            
-            if fast - slow + 1 < min_len:
-                l = slow
-                r = fast
-                min_len = fast - slow + 1
-            
-            fast += 1
+                if s_count[left_char] == t_count[left_char]:
+                    break
+
+                s_count[left_char] -= 1
+                left += 1
         
-        return s[l:r + 1] if min_len != float("inf") else ""
+        return s[result_l: result_r] if min_len != float("inf") else ""
+                
+                
 
-    def get_char_count(self, s):
-        results = {}
-        for char in s:
-            results[char] = results.get(char, 0) + 1
 
-        return results
-            
