eb1e83426a115f2fcab989ea3b1a7f2653e16a10
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-29 21:48:54 -1000

    LC-longest-valid-parentheses [Runtime: 6 ms; Memory: 18.4 MB]

--- stack/longest-valid-parentheses.py
+++ stack/longest-valid-parentheses.py
@@ -1,29 +1,22 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        if not s:
-            return 0
-
-        # record all unmatched (, ) idxs
-        unmatched_idxs = []
-        longest_len = 0
+        stack = [-1]
+        max_len = 0
         for idx in range(len(s)):
             char = s[idx]
             if char == "(":
-                prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
-                longest_len = max(longest_len, idx - prev_unmatched_idx - 1)
-                unmatched_idxs.append(idx)
+                stack.append(idx)
                 continue
             
             # char == ")"
-            if unmatched_idxs and s[unmatched_idxs[-1]] == "(":
-                unmatched_idxs.pop()
-            else:
-                prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
-                longest_len = max(longest_len, idx - prev_unmatched_idx - 1)
-                prev_idx = idx
-                unmatched_idxs.append(idx)
+            if stack:
+                stack.pop()
+            if not stack:
+                stack.append(idx)
+                continue
+            
+            max_len = max(max_len, idx - stack[-1])
 
-        prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
-        longest_len = max(len(s) - prev_unmatched_idx - 1, longest_len)
+        return max_len
 
-        return longest_len
+
