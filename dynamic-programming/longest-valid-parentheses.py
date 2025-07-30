0376769f478c163affe56da94f81b1a0f8b95f87
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-29 21:01:08 -1000

    LC-longest-valid-parentheses [Runtime: 8 ms; Memory: 18.5 MB]

--- dynamic-programming/longest-valid-parentheses.py
+++ dynamic-programming/longest-valid-parentheses.py
@@ -3,10 +3,14 @@ class Solution:
         if not s:
             return 0
 
+        # record all unmatched (, ) idxs
         unmatched_idxs = []
+        longest_len = 0
         for idx in range(len(s)):
             char = s[idx]
             if char == "(":
+                prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
+                longest_len = max(longest_len, idx - prev_unmatched_idx - 1)
                 unmatched_idxs.append(idx)
                 continue
             
@@ -14,14 +18,12 @@ class Solution:
             if unmatched_idxs and s[unmatched_idxs[-1]] == "(":
                 unmatched_idxs.pop()
             else:
+                prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
+                longest_len = max(longest_len, idx - prev_unmatched_idx - 1)
+                prev_idx = idx
                 unmatched_idxs.append(idx)
 
-        prev_idx = -1
-        longest_len = 0
-        for idx in unmatched_idxs:
-            longest_len = max(longest_len, idx - prev_idx - 1)
-            prev_idx = idx
-
-        longest_len = max(len(s) - prev_idx - 1, longest_len)
+        prev_unmatched_idx = unmatched_idxs[-1] if unmatched_idxs else -1
+        longest_len = max(len(s) - prev_unmatched_idx - 1, longest_len)
 
         return longest_len
