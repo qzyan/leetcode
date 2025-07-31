7cc8eda71f31f6b233154321dde547d44fdb669e
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-07-30 21:12:59 -1000

    LC-minimum-remove-to-make-valid-parentheses [Runtime: 37 ms; Memory: 19.2 MB]

--- stack/minimum-remove-to-make-valid-parentheses.py
+++ stack/minimum-remove-to-make-valid-parentheses.py
@@ -3,24 +3,23 @@ class Solution:
         if not s:
             return ""
 
-        unmatched_opens = []
-        unmatched_closes = []
+        unmatched_idxs = []
         for idx in range(len(s)):
-            a = s[idx]
-            if a == "(":
-                unmatched_opens.append(idx)
-            elif a == ")":
-                if unmatched_opens:
-                    unmatched_opens.pop()
-                else:
-                    unmatched_closes.append(idx)
-        
-        results = [c for c in s]
-        for idx in unmatched_opens:
-            results[idx] = ""
-        
-        for idx in unmatched_closes:
-            results[idx] = ""
-
-        return "".join(results)
+            char = s[idx]
+            if char not in "()":
+                continue
             
+            if char == "(":
+                unmatched_idxs.append(idx)
+                continue
+            
+            if unmatched_idxs and s[unmatched_idxs[-1]] == "(":
+                unmatched_idxs.pop()
+            else:
+                unmatched_idxs.append(idx)
+
+        result = [char for char in s]
+        for idx in unmatched_idxs:
+            result[idx] = ""
+
+        return "".join(result)
