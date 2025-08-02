17f2112cb7d08a421d1498f3a43c02a7b2aa4489
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-02 09:21:04 -1000

    LC-copy-list-with-random-pointer [Runtime: 42 ms; Memory: 18.5 MB]

--- linked-list/copy-list-with-random-pointer.py
+++ linked-list/copy-list-with-random-pointer.py
@@ -9,26 +9,43 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
-
-        dummy = Node(0)
-        tail = dummy
-        mapping = {}
         curr_node = head
         while curr_node:
-            copy = Node(curr_node.val)
-            tail.next = copy
-            tail = copy
-            mapping[curr_node] = copy
+            next_node = curr_node.next
+            copy_node = Node(curr_node.val)
+            
+            curr_node.next = copy_node
+            copy_node.next = next_node
 
-            curr_node = curr_node.next
+            curr_node = next_node
         
         curr_node = head
         while curr_node:
-            if curr_node.random:
-                mapping[curr_node].random = mapping[curr_node.random]
+            copy_node = curr_node.next
+            random_node = curr_node.random
+            if random_node:
+                copy_node.random = random_node.next
 
-            curr_node = curr_node.next
+            curr_node = copy_node.next
+
+        dummy = Node(0)
+        tail = dummy
+        curr_node = head
+        while curr_node:
+            copy_node = curr_node.next
+            next_node = copy_node.next
+
+            tail.next = copy_node
+            tail = copy_node
+
+            if next_node:
+                copy_node.next = next_node.next
+
+            curr_node.next = next_node
+            curr_node = next_node
 
         return dummy.next
+
+
+
+
