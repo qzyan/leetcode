e77b1ccbdd4da4705e3501964e0bcabc3794f127
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-04 12:36:37 -1000

    LC-task-scheduler [Runtime: 146 ms; Memory: 20.1 MB]

--- hash-table/task-scheduler.py
+++ hash-table/task-scheduler.py
@@ -1,18 +1,26 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         task_count = Counter(tasks)
-        max_count = max(task_count.values())
+        print(task_count)
+        max_heap = [(-c, task) for task, c in task_count.items()]
+        heapq.heapify(max_heap)
+        seq = []
 
-        idle_count = (max_count - 1) * (n + 1)
-        for task, count in task_count.items():
-            idle_count -= min(count, max_count - 1)
-            if idle_count <= 0:
-                break
+        while max_heap:
+            wait_list = []
+            for _ in range(n + 1):
+                if max_heap:
+                    count, task = heapq.heappop(max_heap)
+                    count = -count
+                    seq.append(task)
+                    count -= 1
+                    if count:
+                        wait_list.append((-count, task))
+                else:
+                    if wait_list:
+                        seq.append("idle")
 
-        if idle_count <= 0:
-            return sum(task_count.values())
-        
-        return sum(task_count.values()) + idle_count
+            while wait_list:
+                heapq.heappush(max_heap, wait_list.pop())
 
-            
-
+        return len(seq)
