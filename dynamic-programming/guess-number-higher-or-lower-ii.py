576d87248005459a3d23449bb431f6b76f34d37c
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-05 16:51:31 -1000

    LC-guess-number-higher-or-lower-ii [Runtime: 3918 ms; Memory: 20.2 MB]

--- dynamic-programming/guess-number-higher-or-lower-ii.py
+++ dynamic-programming/guess-number-higher-or-lower-ii.py
@@ -1,22 +1,25 @@
-''' 
-    n-3
-   /    \ 
-        n-1
-       /    \
-      n-2    n
-'''
+# fn(1, n) = min
+#   1 + max(fn(1, 0), fn(2, n)), 
+#   2 + max(fn(1, 1), fn(3, n)),
+#.  3 + max(fn(1, 2), fn(4, n)),
+#.  .....
+#.  n + max(fn(1, n - 1), fn(n + 1, n))
+
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
-        for row in range(n, 0, -1):
-            for col in range(row + 1, n + 1):
-                res = float("inf")
-                for k in range(row, col):
-                    opt1 = dp[row][k - 1]
-                    opt2 = dp[k + 1][col]
-                    cost = max(opt1, opt2) + k
-                    res = min(cost, res)
+        return self.dfs(1, n, {})
 
-                dp[row][col] = res
+    def dfs(self, start, end, memo):
+        if (start, end) in memo:
+            return memo[(start, end)]
 
-        return dp[1][n]
+        if start >= end:
+            return 0
+        
+        res = float("inf")
+        for i in range(start, end + 1):
+            curr_res = i + max(self.dfs(start, i - 1, memo), self.dfs(i + 1, end, memo))
+            res = min(res, curr_res)
+
+        memo[(start, end)] = res
+        return res
