61d7b35b847a017a7b03264cf3113fdb7954ada0
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-05 17:36:08 -1000

    LC-ipo [Runtime: 407 ms; Memory: 47.5 MB]

--- sorting/ipo.py
+++ sorting/ipo.py
@@ -1,18 +1,24 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        max_profits = []
-        capital = [(cap, idx) for idx, cap in enumerate(capital)]
-        heapq.heapify(capital)
+        n = len(profits)
+        c_ps = [(capital[idx], profits[idx]) for idx in range(n)]
+        c_ps = sorted(c_ps) # [(c, p), ....]
+        idx = 0
+        max_heap = []
         while k > 0:
-            while capital and capital[0][0] <= w:
-                cap, idx = heapq.heappop(capital)
-                profit = profits[idx]
-                heapq.heappush(max_profits, -profit)
-            if max_profits:
-                w -= heapq.heappop(max_profits)
-            else:
-                return w
+            # push all achievable projects in max_heap
+            while idx < n and c_ps[idx][0] <= w:
+                heapq.heappush(max_heap, (-c_ps[idx][1], c_ps[idx][0]))
+                idx += 1
+
+            if not max_heap:
+                break
+            
+            # work on the most profitable project
+            p, c = heapq.heappop(max_heap)
+            p = -p
+            w += p
 
             k -= 1
-        
+
         return w
