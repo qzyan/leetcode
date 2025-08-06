000347e6e049beef47ca7afc376a348d5a7f1701
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-05 17:19:47 -1000

    LC-guess-number-higher-or-lower-ii [Runtime: 1518 ms; Memory: 18.7 MB]

--- game-theory/guess-number-higher-or-lower-ii.py
+++ game-theory/guess-number-higher-or-lower-ii.py
@@ -1,25 +1,20 @@
-# fn(1, n) = min
-#   1 + max(fn(1, 0), fn(2, n)), 
-#   2 + max(fn(1, 1), fn(3, n)),
-#.  3 + max(fn(1, 2), fn(4, n)),
-#.  .....
-#.  n + max(fn(1, n - 1), fn(n + 1, n))
-
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
-        return self.dfs(1, n, {})
-
-    def dfs(self, start, end, memo):
-        if (start, end) in memo:
-            return memo[(start, end)]
-
-        if start >= end:
+        if n <= 1:
             return 0
-        
-        res = float("inf")
-        for i in range(start, end):
-            curr_res = i + max(self.dfs(start, i - 1, memo), self.dfs(i + 1, end, memo))
-            res = min(res, curr_res)
 
-        memo[(start, end)] = res
-        return res
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for row in range(n, 0, -1):
+            for col in range(row + 1, n + 1):
+                dp[row][col] = self.calc_cost(dp, row, col)
+
+        return dp[1][n]
+
+    def calc_cost(self, dp, row, col):
+        min_cost = float("inf")
+        for i in range(row, col):
+            cost = i + max(dp[row][i - 1], dp[i + 1][col])
+            min_cost = min(min_cost, cost)
+
+        return min_cost
