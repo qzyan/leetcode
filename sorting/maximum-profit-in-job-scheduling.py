4521209db8af5c1ded33d2d5ebe50f948a68a3e4
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-06 17:49:38 -1000

    LC-maximum-profit-in-job-scheduling [Runtime: 346 ms; Memory: 33.3 MB]

--- sorting/maximum-profit-in-job-scheduling.py
+++ sorting/maximum-profit-in-job-scheduling.py
@@ -2,42 +2,37 @@ class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         n = len(startTime)
         profits = [(startTime[i], endTime[i], profit[i]) for i in range(n)]
-        profits = sorted(profits)
-        return self.dfs(0, profits, {})
+        # sort by endtime
+        profits = sorted(profits, key=lambda x: (x[1], x[0], x[2]))
+        dp = [0] * n
 
-    def dfs(self, curr_idx, profits, memo):
-        if curr_idx == len(profits):
-            return 0
+        for idx in range(n):
+            dp[idx] = max(dp[idx], dp[idx - 1]) if idx - 1 >= 0 else 0 # skip this job
+            last_end_before_idx = self.find_last_end_before_job(profits, profits[idx][0])
 
-        if curr_idx in memo:
-            return memo[curr_idx]
+             # take this job and add prev
+            if last_end_before_idx == -1:
+                dp[idx] = max(dp[idx], profits[idx][2])
+            else:
+                dp[idx] = max(dp[idx], dp[last_end_before_idx] + profits[idx][2])
 
-        curr_start, curr_end, curr_profit = profits[curr_idx]
-        # take this job
-        next_workable_idx = self.find_first_start_after(profits, curr_end)
-        profit1 = curr_profit + self.dfs(next_workable_idx, profits, memo)
-        # skip this job
-        profit2 = self.dfs(curr_idx + 1, profits, memo)
+        return dp[-1]
 
-        memo[curr_idx] = max(profit1, profit2)
-        return max(profit1, profit2)
-
-    def find_first_start_after(self, profits, curr_end):
+    def find_last_end_before_job(self, profits, start_time):
         left = 0
         right = len(profits) - 1
+
         while left + 1 < right:
             mid = (left + right) // 2
-            if profits[mid][0] >= curr_end:
-                right = mid
-            else:
+            if profits[mid][1] <= start_time:
                 left = mid
+            else:
+                right = mid
 
-        if profits[left][0] >= curr_end:
-            return left
-        
-        if profits[right][0] >= curr_end:
+        if profits[right][1] <= start_time:
             return right
         
-        return len(profits)
-
+        if profits[left][1] <= start_time:
+            return left
         
+        return -1
