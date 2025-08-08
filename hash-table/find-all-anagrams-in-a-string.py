9a154fc03a66858975035d26888311d19cb167a0
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-07 21:04:03 -1000

    LC-find-all-anagrams-in-a-string [Runtime: 209 ms; Memory: 18.3 MB]

--- hash-table/find-all-anagrams-in-a-string.py
+++ hash-table/find-all-anagrams-in-a-string.py
@@ -2,35 +2,34 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(s) < len(p):
             return []
-        
-        p_count = [0] * 26
+
+        p_char_counts = [0] * 26
         for char in p:
-            idx = ord(char) - ord("a")
-            p_count[idx] += 1
-
-        s_count = [0] * 26
+            p_char_counts[ord(char) - ord("a")] += 1
         
+        p_code = self.get_code(p_char_counts)
+        
+        s_char_counts = [0] * 26
         for idx in range(len(p)):
-            char = s[idx]
-            s_count[ord(char) - ord("a")] += 1
-
-        p_code = tuple(p_count)
-        s_code = tuple(s_count)
+            s_char_counts[ord(s[idx]) - ord("a")] += 1
+        
         results = []
+        s_code = self.get_code(s_char_counts)
         if p_code == s_code:
             results.append(0)
 
         for idx in range(len(p), len(s)):
-            char_add = s[idx]
-            char_remove = s[idx - len(p)]
-            s_count[ord(char_add) - ord("a")] += 1
-            s_count[ord(char_remove) - ord("a")] -= 1
-
-            s_code = tuple(s_count)
+            prev_char = s[idx - len(p)]
+            s_char_counts[ord(prev_char) - ord("a")] -= 1
+            curr_char = s[idx]
+            s_char_counts[ord(curr_char) - ord("a")] += 1
+            
+            s_code = self.get_code(s_char_counts)
             if p_code == s_code:
                 results.append(idx - len(p) + 1)
 
         return results
 
-        
 
+    def get_code(self, char_counts):
+        return "".join([str(count) for count in char_counts])
