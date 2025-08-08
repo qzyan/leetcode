a233a1385bef35c6bf5f96fcb9e0cddf4b2222e1
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-07 19:53:51 -1000

    LC-top-k-frequent-words [Runtime: 4 ms; Memory: 18.1 MB]

--- trie/top-k-frequent-words.py
+++ trie/top-k-frequent-words.py
@@ -1,26 +1,28 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        word_count = Counter(words)
+        min_heap = []
+        for word, count in word_count.items():
+            ele = Element(word, count)
+            heapq.heappush(min_heap, ele)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        results = []
+        while min_heap:
+            word = heapq.heappop(min_heap).word
+            results.append(word)
+
+        results.reverse()
+        return results
+
 class Element:
-    def __init__(self, count, word):
-        self.count = count
+    def __init__(self, word, count):
         self.word = word
+        self.count = count
 
     def __lt__(self, other):
         if self.count != other.count:
             return self.count < other.count
-
-        return self.word > other.word
-
-
-class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        heap = [] # keep the largest elements, remove smaller
-        word_count_map = Counter(words)
-
-        for word, count in word_count_map.items():
-            heapq.heappush(heap, Element(count, word))
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        sorted_elements = sorted(heap, key=lambda ele: (-ele.count, ele.word))
-        return [ele.word for ele in sorted_elements]
-
-
+        
+        return other.word < self.word
