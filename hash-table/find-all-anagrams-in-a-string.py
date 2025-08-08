a1fabbac19f2374c792ad582d42d1c28db2fca1e
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-07 21:09:42 -1000

    LC-find-all-anagrams-in-a-string [Runtime: 37 ms; Memory: 18.5 MB]

--- hash-table/find-all-anagrams-in-a-string.py
+++ hash-table/find-all-anagrams-in-a-string.py
@@ -5,31 +5,27 @@ class Solution:
 
         p_char_counts = [0] * 26
         for char in p:
-            p_char_counts[ord(char) - ord("a")] += 1
-        
-        p_code = self.get_code(p_char_counts)
-        
+            p_char_counts[self.get_idx(char)] += 1
+                
         s_char_counts = [0] * 26
         for idx in range(len(p)):
-            s_char_counts[ord(s[idx]) - ord("a")] += 1
+            char = s[idx]
+            s_char_counts[self.get_idx(char)] += 1
         
         results = []
-        s_code = self.get_code(s_char_counts)
-        if p_code == s_code:
+        if p_char_counts == s_char_counts:
             results.append(0)
 
         for idx in range(len(p), len(s)):
             prev_char = s[idx - len(p)]
-            s_char_counts[ord(prev_char) - ord("a")] -= 1
+            s_char_counts[self.get_idx(prev_char)] -= 1
             curr_char = s[idx]
-            s_char_counts[ord(curr_char) - ord("a")] += 1
+            s_char_counts[self.get_idx(curr_char)] += 1
             
-            s_code = self.get_code(s_char_counts)
-            if p_code == s_code:
+            if p_char_counts == s_char_counts:
                 results.append(idx - len(p) + 1)
 
         return results
 
-
-    def get_code(self, char_counts):
-        return "".join([str(count) for count in char_counts])
+    def get_idx(self, char):
+        return ord(char) - ord("a")
