5b05da2ff6016ae5194a1a694b375d823ceba5f0
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-26 21:37:01 -1000

    LC-design-add-and-search-words-data-structure [Runtime: N/A; Memory: N/A]

---
+++ trie/design-add-and-search-words-data-structure.py
@@ -0,0 +1,65 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        self.word = ""
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+                continue
+            
+            new_node = TrieNode()
+            node.children[char] = new_node
+            node = new_node
+
+        node.word = word
+        node.is_word = True
+
+class WordDictionary:
+    def __init__(self):
+        self.trie = Trie()
+        
+    def addWord(self, word: str) -> None:
+        self.trie.add(word)
+
+    def search(self, word: str) -> bool:
+        node = self.trie.root
+
+        return self.helper(node, word, 0)
+
+    def helper(self, node, word, start_idx):
+        if start_idx == len(word):
+            return True
+
+        for idx in range(start_idx, len(word)):
+            char = word[idx]
+            if char != ".":
+                if char in node.children:
+                    node = node.children[char]
+                else:
+                    return False
+            else:
+                for next_char, next_node in node.children.items():
+                    if self.helper(next_node, word, idx + 1):
+                        return True
+                
+                return False
+
+        return node.is_word      
+
+
+
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
