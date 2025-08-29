751f5035fbca985518efafb80f4d04c3924dd033
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-08-28 21:09:22 -1000

    LC-letter-combinations-of-a-phone-number [Runtime: 0 ms; Memory: 17.7 MB]

--- string/letter-combinations-of-a-phone-number.py
+++ string/letter-combinations-of-a-phone-number.py
@@ -1,5 +1,8 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
         digit_chars = {
             "2": "abc",
             "3": "def",
@@ -11,15 +14,18 @@ class Solution:
             "9": "wxyz"
         }
 
-        results = deque([])
-        for digit in digits:
-            if not results:
-                for char in digit_chars[digit]:
-                    results.append(char)
-            else:
-                for _ in range(len(results)):
-                    curr_comb = results.popleft()
-                    for char in digit_chars[digit]:
-                        results.append(curr_comb + char)
+        path = []
+        paths = []
+        self.dfs(digits, 0, path, paths, digit_chars)
+        return paths
 
-        return list(results)
+    def dfs(self, digits, curr_idx, path, paths, digit_chars):
+        if curr_idx == len(digits):
+            paths.append("".join(path))
+            return
+
+        digit = digits[curr_idx]
+        for char in digit_chars[digit]:
+            path.append(char)
+            self.dfs(digits, curr_idx + 1, path, paths, digit_chars)
+            path.pop()
