8511c266e9d96954e47b568518571e18d782ba5f
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-09-04 01:29:17 -1000

    LC-combination-sum [Runtime: 15 ms; Memory: 17.9 MB]

--- backtracking/combination-sum.py
+++ backtracking/combination-sum.py
@@ -1,20 +1,28 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        path = []
-        paths = []
-        self.dfs(candidates, target, path, paths, 0, 0)
-        return paths
+        candidates = sorted(candidates)
+        comb = []
+        combs = []
+        self.dfs(comb, combs, target, 0, candidates)
+        return combs
 
-    def dfs(self, candidates, target, path, paths, start_idx, curr_total):
-        if curr_total == target:
-            paths.append(path[:])
+    def dfs(self, comb, combs, curr_target, start_idx, candidates):
+        if curr_target == 0:
+            combs.append(comb[:])
             return
         
-        if curr_total > target:
+        if curr_target < 0:
             return
 
         for idx in range(start_idx, len(candidates)):
-            num = candidates[idx]
-            path.append(num)
-            self.dfs(candidates, target, path, paths, idx, curr_total + num)
-            path.pop()
+            if idx > 0 and candidates[idx] == candidates[idx - 1]:
+                continue
+            
+            comb.append(candidates[idx])
+            self.dfs(comb, combs, curr_target - candidates[idx], idx, candidates)
+            comb.pop()
+            
+
+
+        
+
