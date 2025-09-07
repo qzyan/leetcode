8fe1fa89bbb190a9e7bc7a8adc1f0a041a78e7e7
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-09-06 22:15:32 -1000

    LC-construct-quad-tree [Runtime: 85 ms; Memory: 18.6 MB]

--- array/construct-quad-tree.py
+++ array/construct-quad-tree.py
@@ -19,19 +19,17 @@ class Solution:
         is_leaf = True
         val = grid[tlr][tlc]
         for row in range(tlr, brr + 1):
-            for col in range(tlc, brc):
+            for col in range(tlc, brc + 1):
                 if grid[row][col] != val:
                     is_leaf = False
-        
-        if is_leaf:
-            return Node(val, is_leaf, None, None, None, None)
-        
-        mid_row = (tlr + brr) // 2
-        mid_col = (tlc + brc) // 2
-        tl = self.helper(grid, tlr, tlc, mid_row, mid_col)
-        tr = self.helper(grid, tlr, mid_col + 1, mid_row, brc)
-        bl = self.helper(grid, mid_row + 1, tlc, brr, mid_col)
-        br = self.helper(grid, mid_row + 1, mid_col + 1, brr, brc)
+                    mid_row = (tlr + brr) // 2
+                    mid_col = (tlc + brc) // 2
+                    tl = self.helper(grid, tlr, tlc, mid_row, mid_col)
+                    tr = self.helper(grid, tlr, mid_col + 1, mid_row, brc)
+                    bl = self.helper(grid, mid_row + 1, tlc, brr, mid_col)
+                    br = self.helper(grid, mid_row + 1, mid_col + 1, brr, brc)
 
-        return Node(val, is_leaf, tl, tr, bl, br)
-
+                    return Node(val, is_leaf, tl, tr, bl, br)
+            
+        return Node(val, is_leaf, None, None, None, None)
+        
