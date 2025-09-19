6ce4d8179fcff10005e8cba77f09257a0369c3d9
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-09-18 16:20:51 -1000

    LC-find-median-from-data-stream [Runtime: 158 ms; Memory: 40.8 MB]

--- sorting/find-median-from-data-stream.py
+++ sorting/find-median-from-data-stream.py
@@ -5,45 +5,37 @@ class MedianFinder:
         self.min_heap = []
 
     def addNum(self, num: int) -> None:
-        # if max_h []: append to max_h
-        # if num <= max_h max: append to max_h
-        # if num > max_h max: append to min_h
-        # rebalance
-        max_h = self.max_heap
-        min_h = self.min_heap
-        if not max_h:
-            heapq.heappush(max_h, -num)
+        if not self.max_heap:
+            heapq.heappush(self.max_heap, -num)
             return
-
-        if num <= -max_h[0]:
-            heapq.heappush(max_h, -num)
+        
+        if -self.max_heap[0] >= num:
+            heapq.heappush(self.max_heap, -num)
         else:
-            heapq.heappush(min_h, num)
+            heapq.heappush(self.min_heap, num)
 
         self.rebalance()
 
+    def rebalance(self):
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            num = -heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, num)
+            return
+        
+        if len(self.max_heap) < len(self.min_heap):
+            num = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -num)
+
     def findMedian(self) -> float:
+        if not self.max_heap and not self.min_heap:
+            return 0.0
+
         if len(self.max_heap) == len(self.min_heap):
             return (self.min_heap[0] - self.max_heap[0]) / 2
-
-        return float(-self.max_heap[0])
+        else:
+            return float(-self.max_heap[0])
         
-    def rebalance(self):
-        # if len(max_h) < len(min_h): pop one from min_h and add to max_h
-        # if len(max_h) + 1 > len(min_h): pop one from max_h and add to min_h
-        max_h = self.max_heap
-        min_h = self.min_heap
 
-        if len(max_h) < len(min_h):
-            num = heapq.heappop(min_h)
-            heapq.heappush(max_h, -num)
-            return
-
-        if len(max_h) > len(min_h) + 1:
-            num = heapq.heappop(max_h)
-            num = -num
-            heapq.heappush(min_h, num)
-            
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
