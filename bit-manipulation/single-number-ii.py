cae7c19faea48a692c11093a0ccc3b8c9b8187c6
QingzhouYan <94769046+qzyan@users.noreply.github.com>
2025-09-26 08:26:38 -1000

    LC-single-number-ii [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/single-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        bit_count = [0] * 32
+        for num in nums:
+            for i in range(32):
+                bit = num & 1
+                num = num >> 1
+                if bit == 1:
+                    bit_count[i] += 1
+
+        res = 0
+        for bit, count in enumerate(bit_count):
+            if count % 3 != 0:
+                res += 1 << bit
+
+        return res
